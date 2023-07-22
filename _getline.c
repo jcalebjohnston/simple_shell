bbb9471089221e48ec1bef8ef6134063a11cc9d0
Joseph Caleb Johnston <josephcalebjohnston@gmail.com>
2023-07-22 15:58:51 -0700

    reads line from an output_betty

---
+++ _getline.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+/**
+ * _getline - A function that reads a line from standard input
+ *
+ * Return: On success, returns a pointer to the read line.
+ * On failure or when there's nothing more to read, returns NULL.
+ */
+char *_getline(void)
+{
+    static char buffer[READ_SIZE];
+    static ssize_t buffer_pos = 0,bytes_read = 0;
+
+    char *line = NULL;
+    ssize_t line_size = 0, t = 0, line_len, new_line_size;
+
+    while (1)
+    {
+        if (buffer_pos >= bytes_read)
+        {
+            bytes_read = read(STDIN_FILENO, buffer, READ_SIZE);
+            if (bytes_read <= 0)
+                return (line);
+            buffer_pos = 0;
+        }
+        for (t = buffer_pos; t < bytes_read; t++)
+        {
+            if (buffer[t] == '\n')
+                break;
+        }
+        ssize_t line_len = t - buffer_pos;
+        ssize_t new_line_size = line_size + line_len + 1;
+
+        line = realloc(line, new_line_size);
+        if (line == NULL)
+            return (NULL);
+        for (t = 0; buffer_pos < bytes_read && buffer[buffer_pos] != '\n'; t++)
+        {
+            line[line_size + t] = buffer[buffer_pos];
+            buffer_pos++;
+        }
+        line[line_size + t] = '\0';
+        line_size = new_line_size;
+
+        if (buffer_pos < bytes_read || line_len > 0)
+            return (line);
+    }
+}
